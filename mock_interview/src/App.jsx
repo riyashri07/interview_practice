0c335ea66fa0364a8ce8c222d90b821be4e74aff
riyashri07 <riyampct7@gmail.com>
2023-05-03 18:13:57 +0530

    push

--- mock_interview/src/App.jsx
+++ mock_interview/src/App.jsx
@@ -1,26 +1,48 @@
 // usememo example
 
-import "./App.css";
-import React, { useState } from "react";
-function App() {
-  const [count, setData] = useState(1);
-  const [item, setItem] = useState(20);
+// import "./App.css";
+// import React, { useState } from "react";
+// function App() {
+//   const [count, setData] = useState(1);
+//   const [item, setItem] = useState(20);
 
-  const newApple = React.useMemo(
-    function appleTime() {
-      console.warn("Hello");
-      return 100 * count;
-    },
-    [item]
-  );
+//   const newApple = React.useMemo(
+//     function appleTime() {
+//       console.warn("Hello");
+//       return 100 * count;
+//     },
+//     [item]
+//   );
+//   return (
+//     <div className="App">
+//       <h1>Hooks in React {count}</h1>
+//       {newApple}
+//       <button onClick={() => setData(count + 1)}>Update State</button>
+//       <button onClick={() => setItem(item * 10)}>Update State</button>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+
+import React, { useState } from 'react'
+
+const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div className="App">
-      <h1>Hooks in React {count}</h1>
-      {newApple}
-      <button onClick={() => setData(count + 1)}>Update State</button>
-      <button onClick={() => setItem(item * 10)}>Update State</button>
+    <div>
+      <h3>Count:{count}</h3>
+      <button onClick={() => setCount(count + 1)}>Add</button>
+      <button
+        disabled={count == 0 ? true : false}
+        onClick={() => setCount(count - 1)}
+      >
+        Reduce
+      </button>
     </div>
   );
 }
 
-export default App;
+export default App
--- second_largest/index.js
+++ second_largest/index.js
@@ -1,16 +1,51 @@
-let arr = [2, 3, 5, 6, 57, 58, 9];
-let max = -Infinity;
-let secondmax = -Infinity;
+// let arr = [2, 3, 5, 6, 58, 9];
+// let max = -Infinity;
+// let secondmax = -Infinity;
 
-for (let i = 0; i < arr.length; i++){
-    if (arr[i] > max) {
-        secondmax = max;
-        max = arr[i];
-    }
-}
-console.log(secondmax )
+// for (let i = 0; i < arr.length; i++){
+//     if (arr[i] > max) {
+//         secondmax = max;
+//         max = arr[i];
+//     }
+// }
+// console.log(secondmax )
+
+// let arr = [1, 2, 3, 4, 5, 66, 6, 7,8,66,66, 66]
+// arr.sort((a, b) => a - b)
+// for ( let i=arr.length-1;i>=0;i--){
+// 	if(arr[i]<arr[arr.length-1]){
+// 		console.log(arr[i])
+// 		break;
+// 	}
+// }
+
+// let obj={};
+// for(let i=0;i<arr.length;i++){
+// 	obj[arr[i]]=1
+// }
+// // console.log(obj)
+// let ans=[]
+// for(let key in obj){
+// 	ans.push(key)
+// }
+// console.log(+ans[ans.length-2])
 
 
+// let arr = [1, 5, 66, 7,8,9,66,66, 66]
+// let max= -Infinity
+// let sMax=-Infinity
+
+// for(let i=0;i<arr.length;i++){
+
+// 	if(arr[i]>max){
+// 		sMax=max  
+// 		max=arr[i]
+// 	}
+// 	if(arr[i]>sMax&&arr[i]<max){
+// 		sMax=arr[i]
+// 	}
+// }
+// console.log(sMax,)
 
 
 
@@ -40,3 +75,30 @@ console.log(secondmax )
 // const arr = [1, 5, 2, 8, 3, 10];
 // const secondLargest = findSecondLargestNumber(arr);
 // console.log(secondLargest); // Output: 8
+
+// console.log(x);
+// var x = "5";
+
+// console.log(y);
+// let y = "5";
+
+
+// const z = "5";
+// z = "10";
+// console.log(z);
+// by()
+// hello();
+
+
+// function by() {
+//     hello()
+//   function hello() {
+//     console.log("riya");
+//   }
+// }
+
+function random(max, min) {
+    return Math.floor(Math.random() * (max - min));
+}
+let x = random(100, 1);
+console.log(x);
